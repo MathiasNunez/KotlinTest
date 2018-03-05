acb8b979029d87395de765aa1680d3790dca9fa9
Mathias Nuñez <mathiasnu@gmail.com>
2018-03-05 14:46:29 -0300

    Added test for using synthetic import and extensions

--- app/src/main/java/com/kotlin/test/fragments/FirstFragment.kt
+++ app/src/main/java/com/kotlin/test/fragments/FirstFragment.kt
@@ -6,8 +6,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import com.kotlin.test.R
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.fragment_kotlin_example.*
 
 /**
  * Created by mnunez on 2/6/18.
@@ -18,15 +19,20 @@ class FirstFragment : Fragment() {
     var activityCallback: FirstFragment.FragmentListener? = null
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater?.inflate(R.layout.fragment_kotlin_example,
+        return inflater?.inflate(R.layout.fragment_kotlin_example,
                 container, false)
+    }
 
-        val fragmentCallbackButton = view?.findViewById<Button>(R.id.fragmentButton)
-        fragmentCallbackButton?.setOnClickListener {
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fragmentButton.setOnClickListener{
             activityCallback?.onFragmentButtonClick()
         }
 
-        return view
+        text.text = "Changed in code using Kotlin Extensions"
+        
+
     }
 
     override fun onAttach(context: Context?) {
